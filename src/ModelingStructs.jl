62f22b873c7de01085f37c958417b6daa5ac6cb7
Jonnie Diegelman <47193959+jonniedie@users.noreply.github.com>
2020-03-09 16:15:06 -0400

    s

--- src/ModelingStructs.jl
+++ src/ModelingStructs.jl
@@ -6,7 +6,7 @@ include("types.jl")
 
 function __init__()
     @require Optim = "429524aa-4258-5aef-a3af-852621145aeb" include("..\\patches\\optim.jl")
-    @require NLSolversBase = "d41bc354-129a-5804-8e4c-c37616107c6c"
+    # @require NLSolversBase = "d41bc354-129a-5804-8e4c-c37616107c6c"
 end
 
 export ModelingStruct, mstruct
